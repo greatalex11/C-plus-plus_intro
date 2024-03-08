b2f15d3e82383a3055dd53a78b2136ca7ae51109
Alex <alexguerillot1@gmail.com>
2024-03-08 15:19:51 +0100

    lecture2: varialbes

---
+++ bases.cpp
@@ -0,0 +1,57 @@
+/*
+Lecture 2
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//std::cout <<"Hello world!";	
+	cout <<"Hello world!!";
+	
+	// variables & entrée de valeur au clavier
+	
+	int numb1, numb2, result;
+	
+	// numb1 = 1; init valeur
+	
+    cout << "entrez le nombre 1" << endl;
+    cin >> numb1;
+    
+    cout << "entrez le nombre 2" << endl;
+    cin >> numb2;
+    
+    result = numb1 / numb2;
+	cout << "votre réponse est" << endl;
+	
+	cout << result;	
+	
+	//cout << numb1;	
+	
+	return 0;
+}
+
+
+
+/*
+Lecture 1 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//std::cout <<"Hello world!";
+	
+	cout <<"Hello world!!";
+	
+	return 0;
+}
+
+
+/*
+essai 1
+*/
